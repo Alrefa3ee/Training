8a3111e1f7caaf7dd22e3b012fb806603a77cf55
HazemMeqdad <hazemmeqdad@gmail.com>
2022-09-20 19:31:52 +0300

    Worng Try

--- 2018/problemI.py
+++ 2018/problemI.py
@@ -1,5 +1,8 @@
 # Worng answer
 
+import typing as t
+
+
 c = 5  # عدد الخلاية الكلي
 n = [1, 1, 0, 1, 0]  # حالة الخلاية
 k = 3  # عدد الدورات
@@ -8,11 +11,10 @@ n = [bool(i) for i in n] * k  # تكرار الدورات
 
 steps = 0
 position = 0
-
 for i in range(k):
-    print(n[position+2:4])
-    n[position+2:5] = [False for i in n[position+2:4]]
-    n[position+2:5][-1] = True
+    n[position+2:4] = [False for i in n[position+2:4]]
+    n[position+4] = True
     position += 3
 
-print([int(i) for i in n[-c:]])
+# print([int(i) for i in n[]])
+print([int(i) for i in n])
